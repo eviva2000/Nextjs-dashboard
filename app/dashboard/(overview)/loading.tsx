bb849eb66127a07303e732c4963e8a470bac8ce2
eviva2000 <arjomandielaheh@gmail.com>
2026-01-07 21:59:48 +0100

    add streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+ //By moving page.tsx and loading.tsx in overview folder the loading.tsx file will only apply to dashboard overview page
+ import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
